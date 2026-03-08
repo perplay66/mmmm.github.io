b1d3f7e5c8e42528c755bbc9273dce781075f4bf
perplay66 <plfl@discente.ifpe.edu.br>
2026-03-08 16:38:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const numero = document.getElementById("numeroCafe")
+const botao = document.getElementById("maisCafe")
+
+const VALOR_INICIAL = 447
+const STORAGE_KEY = "cafesTomados"
+
+let cafes = parseInt(localStorage.getItem(STORAGE_KEY))
+
+if(isNaN(cafes)){
+cafes = VALOR_INICIAL
+localStorage.setItem(STORAGE_KEY, cafes)
+}
+
+numero.textContent = cafes
+
+botao.addEventListener("click", () => {
+
+cafes++
+
+numero.textContent = cafes
+
+localStorage.setItem(STORAGE_KEY, cafes)
+
+numero.animate([
+{transform:"scale(1.3)"},
+{transform:"scale(1)"}
+],{duration:250})
+
+if(cafes === 500){
+alert("☕ 500 cafés de amizade conquistados!")
+}
+
+})
+
+})
